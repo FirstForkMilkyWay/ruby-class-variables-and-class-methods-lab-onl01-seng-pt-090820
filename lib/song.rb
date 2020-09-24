ba76bd7207d321589837efb3d56201313fcd8b56
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-24 12:50:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,19 @@ class Song
   
   attr_accessor :name, :artist, :genre 
   
-  
+  @@artists = []
+  @@genres = []
   @@count = 0 
  
-  def initialize(name, artist, genre)
+  def initialize
     @@count += 1
   end
  
   def self.count
     @@count
   end
+  
+  
+  
+  
 end
